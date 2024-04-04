279f8a0e5d20f3f69c60a8ebb1868d44ed9344e1
abdou <sani_a@etna-alternance.net>
2024-04-04 11:56:16 +0200

    firestore data user

--- app/src/services/Auth.jsx
+++ app/src/services/Auth.jsx
@@ -1,15 +1,37 @@
-import { auth } from '../firebase/firebase';
+import { app, auth } from "../firebase/firebase"
+import { getFirestore, collection, setDoc } from "firebase/firestore"
 
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth"
 
-export async function register(email, password) {
-    return createUserWithEmailAndPassword(auth, email, password)
+export async function register(email, password, username) {
+  return createUserWithEmailAndPassword(auth, email, password)
+    .then((userCreated) => {
+      uid: userCreated.user.uid
+      const firestore = getFirestore(app)
+
+      setDoc(collection(firestore, "users").id(uid), {
+        username: username,
+        email: email,
+      })
+    })
+    .then(() => {
+      console.log("Données utilisateur stockées avec succès !")
+    })
+    .catch((err) => {
+      console.log("Erreur: ", err)
+    })
+    .catch((err) => {
+      console.log("Erreur: ", err)
+    }) 
 }
 
 export async function login(email, password) {
-    return signInWithEmailAndPassword(auth, email, password);
+  return signInWithEmailAndPassword(auth, email, password)
 }
 
 export async function signOut() {
-    return auth.signOut();
+  return auth.signOut()
 }
