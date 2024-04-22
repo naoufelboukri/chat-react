518a95244c7f8cb6c2a050737bfc55d656770633
naoufelboukri <naoufelboukri@gmail.com>
2024-04-22 18:25:39 +0200

    deploy

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import { onAuthStateChanged } from 'firebase/auth';
 
 import Messaging from "./pages/Messaging/Messaging.jsx";
